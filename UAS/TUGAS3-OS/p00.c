e919546359ec00455263d37eedd1c41fb624946a
Reka Paska Enda <rekapaskaendaginting@gmail.com>
2020-06-30 19:05:26 +0700

    OS201 rekapaska

--- UAS/TUGAS3-OS/p00.c
+++
@@ -1,9 +0,0 @@
-/*
-// Kontributor : JahnsMichael
-// Tue Jun  9 17:46:47 WIB 2020
- */
-
-#include "share.h"
-char*    akunGitHub = "JahnsMichael";
-int      delay      = 3;
-int      boss       = BOSS;
--- UAS/TUGAS3-OS/p01.c
+++
@@ -1,9 +0,0 @@
-/*
-// Kontributor : rekapaska
-// Tue Jun  9 17:46:47 WIB 2020
- */
-
-#include "share.h"
-char*    akunGitHub = "rekapaska";
-int      delay      = 2;
-int      boss       = NOTBOSS;
